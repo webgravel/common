e13aa1e300138901a52b029da997180ba6a36e0c
Michał Zieliński <michal@zielinscy.org.pl>
2013-07-20 16:25:11 +0200

    upgrade dbtool

--- dbtool.py
+++ dbtool.py
@@ -2,15 +2,27 @@
 import sys
 import graveldb
 
-if len(sys.argv) not in (2, 3, 5):
-    sys.exit('usage: dbtool.py filename [key [property val]]')
+def help():
+    sys.exit('''usage:
+    dbtool.py filename
+    dbtool.py filename key
+    dbtool.py filename key property val
+    dbtool.py filename key --del''')
 
-db = graveldb.TDBShelf(sys.argv[1])
+if len(sys.argv) not in (2, 3, 4, 5):
+    help()
+
+db = graveldb.TDB_BSON_Shelf(sys.argv[1])
 
 if len(sys.argv) == 2:
-    print list(db.iterkeys())
+    print db.keys()
 elif len(sys.argv) == 3:
     print db[sys.argv[2]]
+elif len(sys.argv) == 4:
+    if sys.argv[3] == '--del':
+        del db[sys.argv[2]]
+    else:
+        help()
 elif len(sys.argv) == 5:
     val = db[sys.argv[2]]
     setattr(val, sys.argv[3], eval(sys.argv[4]))
