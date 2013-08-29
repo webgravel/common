23890434760549d441d8fa2836c91d320e32fadb
Michał Zieliński <michal@zielinscy.org.pl>
2013-08-29 21:30:06 +0200

    gravelrpc: SSL in rpctool

--- rpctool.py
+++ rpctool.py
@@ -4,9 +4,15 @@ import gravelrpc
 from gravelrpc import FD
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--ssl', action='store_true')
+parser.add_argument('--ssl-key', default='example.pem')
 parser.add_argument('name')
 parser.add_argument('expression')
 args = parser.parse_args()
 
-client = gravelrpc.Client(args.name)
+if args.ssl:
+    host, port = args.name.split(':')
+    client = gravelrpc.SSLClient((host, int(port)), args.ssl_key)
+else:
+    client = gravelrpc.Client(args.name)
 print eval('client.' + args.expression)
